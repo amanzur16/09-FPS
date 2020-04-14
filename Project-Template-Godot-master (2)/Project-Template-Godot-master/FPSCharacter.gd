dee6e72a89b0a5737f2872be6add6bc363eb40f7
amanzur16 <59851844+amanzur16@users.noreply.github.com>
2020-04-14 18:17:59 -0400

    changes 2

--- Project-Template-Godot-master (2)/Project-Template-Godot-master/FPSCharacter.gd
+++ Project-Template-Godot-master (2)/Project-Template-Godot-master/FPSCharacter.gd
@@ -1,14 +1,16 @@
 extends KinematicBody
 
+var Bullet = preload("res://Bullet.tscn")
 onready var camera = $Pivot/Camera
 
-var gravity = -30
-var jump_speed = 12
+var gravity = -20
+var jump_speed = 6
 var max_speed = 5
 var mouse_sensitivity = 0.002 # radians/pixel
 
 var velocity = Vector3()
 var jump = false
+
 
 func get_input():
 	jump = false
@@ -33,7 +35,13 @@ func _unhandled_input(event):
 		rotate_y(-event.relative.x * mouse_sensitivity)
 		$Pivot.rotate_x(-event.relative.y * mouse_sensitivity)
 		$Pivot.rotation.x = clamp($Pivot.rotation.x, -1.2, 1.2)
-	
+	if event.is_action_pressed("shoot"):
+		var b = Bullet.instance()
+		b.start($Position3D.global_transform)
+		get_parent().add_child(b)
+		
+
+		
 func _physics_process(delta):
 	velocity.y += gravity * delta
 	var desired_velocity = get_input() * max_speed
